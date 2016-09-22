e912b97137acafbc8be647af597f3fb6051867c0
Anthony Gubler <agubler@sitepen.com>
2016-09-22 21:21:58 +0100

    add some basic routing between home-page, cards and card details

--- monster-cards/src/main.ts
+++ monster-cards/src/main.ts
@@ -1,17 +1,53 @@
 import createApp from 'dojo-app/createApp';
 import createMemoryStore from 'dojo-stores/createMemoryStore';
-import createWidget from 'dojo-widgets/createWidget';
+import createRoute, { Route } from 'dojo-routing/createRoute';
+import { Parameters } from 'dojo-routing/interfaces';
+import createRouter from 'dojo-routing/createRouter';
+import createHashHistory from 'dojo-routing/history/createHashHistory';
+import createPanel from 'dojo-widgets/createPanel';
+import createLink from './widgets/common/createLink';
 
 import createNavbar from './widgets/navbar/createNavbar';
 import createFooter from './widgets/footer/createFooter';
 
-let text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis dolor euismod, consequat nisl in, condimentum odio. Vivamus sit amet ipsum volutpat, auctor risus sit amet, consequat libero. Etiam quis suscipit ipsum, sit amet porta lectus. In sollicitudin orci quis tempus bibendum. Nam non ex in est cursus elementum. Praesent imperdiet ante eget leo varius porta. Quisque mollis ipsum eu augue efficitur dignissim. Sed a fermentum metus. In pretium nisi odio, eu aliquam purus interdum eget. Nulla ut mi at felis facilisis porttitor facilisis quis massa. Integer egestas neque sem, non ultrices ex blandit sit amet. Sed eu lectus urna.';
+const router = createRouter();
+router.observeHistory(createHashHistory(), {}, true);
 
-for (let i = 0; i < 5; i++) {
-	text += text;
-}
+const cardsStore = createMemoryStore<any>({
+	data: [
+		{
+			id: 'card-1',
+			name: 'Card 1',
+			description: 'This is card 1, deal with it.',
+			type: 'monster'
+		},
+		{
+			id: 'card-2',
+			name: 'Card 2',
+			description: 'This is card 2, deal with it.',
+			type: 'monster'
+		},
+		{
+			id: 'card-3',
+			name: 'Card 3',
+			description: 'This is card 3, deal with it.',
+			type: 'monster'
+		},
+		{
+			id: 'card-4',
+			name: 'Card 4',
+			description: 'This is card 4, deal with it.',
+			type: 'monster'
+		}
+	]
+});
 
-const widgetStore = createMemoryStore({
+cardsStore.observe().subscribe((options: any) => {
+	// puts all the cards, always into the widget store.
+	return Promise.all(Array.from(options.puts).map((item: any) => widgetStore.patch({id: item.id, type: 'monster-card', label: item.name, href: '#cards/' + item.id })));
+});
+
+const widgetStore = createMemoryStore<any>({
 	data: [
 		{
 			id: 'navbar',
@@ -19,8 +55,7 @@ const widgetStore = createMemoryStore({
 		},
 		{
 			id: 'container',
-			classes: [ 'content' ],
-			label: text
+			classes: [ 'content' ]
 		},
 		{
 			id: 'footer',
@@ -29,7 +64,39 @@ const widgetStore = createMemoryStore({
 	]
 });
 
+const homeRoute: Route<Parameters> = createRoute({
+	exec (request) {
+		return cardsStore.get().then((cards: any) => {
+			const homePage = { id: 'home-page', children: [cards.next().value.id, cards.next().value.id] };
+			return widgetStore.patch(homePage).patch({ id: 'container', children: [ 'home-page' ] });
+		});
+	}
+});
+
+const cardsRoute: Route<Parameters> = createRoute({
+	path: 'cards',
+	exec (request) {
+		return cardsStore.get().then((cards: any) => {
+			const children = Array.from(cards).map((card: any) => card.id);
+			const cardsPage = { id: 'cards-page', children };
+			return widgetStore.patch(cardsPage).patch({ id: 'container', children: [ 'cards-page' ] });
+		});
+	}
+});
+
+const cardDetailRoute: Route<Parameters> = createRoute({
+	path: 'cards/{id}',
+	exec (request: any) {
+		const cardDetailsPage = { id: 'card-details', children: [request.params['id']] };
+		widgetStore.patch(cardDetailsPage).patch({ id: 'container', children: [ 'card-details' ] });
+	}
+});
+
+router.append([homeRoute, cardsRoute, cardDetailRoute]);
+
 const app = createApp({ defaultWidgetStore: widgetStore });
+
+app.registerStore('cards-store', cardsStore);
 
 app.loadDefinition({
 	widgets: [
@@ -39,11 +106,29 @@ app.loadDefinition({
 		},
 		{
 			id: 'container',
-			factory: createWidget
+			factory: createPanel
 		},
 		{
 			id: 'footer',
 			factory: createFooter
+		},
+		{
+			id: 'home-page',
+			factory: createPanel
+		},
+		{
+			id: 'cards-page',
+			factory: createPanel
+		},
+		{
+			id: 'card-details',
+			factory: createPanel
+		}
+	],
+	customElements: [
+		{
+			name: 'monster-card',
+			factory: createLink
 		}
 	]
 });
